9340dcfc1f922a407b1c375ec7cdde12c2a3f971
Sahin, Meryem <meryem.sahin@adesso.com.tr>
2025-02-28 15:03:52 +0300

    init

---
+++ fastlanesample/fastlanesampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  fastlanesampleApp.swift
+//  fastlanesample
+//
+//  Created by Sahin, Meryem on 28.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct fastlanesampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ fastlanesampleTests/fastlanesampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  fastlanesampleTests.swift
+//  fastlanesampleTests
+//
+//  Created by Sahin, Meryem on 28.02.2025.
+//
+
+import Testing
+@testable import fastlanesample
+
+struct fastlanesampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

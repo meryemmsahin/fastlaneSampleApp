7bd3a390e452a6b3827159976c6fd4989d2b36b0
Sahin, Meryem <meryem.sahin@adesso.com.tr>
2025-02-28 22:41:30 +0300

    Fixed API Key authentication issue

--- fastlanesample/ContentView.swift
+++ fastlanesample/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Fastlane Sample App!")
+            Text("Fastlane Sample App!!!")
         }
         .padding()
     }
